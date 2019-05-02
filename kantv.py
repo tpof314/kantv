cb00f398d012a980c0637ca39b05d8e5e800e52c
Haojie Huang <haojie.huang.1989@gmail.com>
2019-05-02 19:19:32 +1000

    update url parser

--- kantv.py
+++ kantv.py
@@ -2,32 +2,36 @@ import requests
 import json
 import sys, os
 
-myURL = "http://www.kantv6.com/index.php/video/play?tvid={}&part_id={}&line=1&seo={}"
+
+myURL1 = "http://www.kantv6.com/index.php/video/play?tvid={}&line=1&seo={}"
+myURL2 = "http://www.kantv6.com/index.php/video/play?tvid={}&part_id={}&line=1&seo={}"
 fake_header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
 
 def reconstruct_link(url):
-	url = url.replace("https://", "")
-	url = url.replace("http://", "")
-	parts = url.split("/")
-	seo   = parts[-2]
-	parts = parts[-1].split("-")
-	tvid    = parts[0]
-	part_id = parts[1]
-	link = myURL.format(tvid, part_id, seo)
-	return link
+    url = url.replace("https://", "")
+    url = url.replace("http://", "")
+    parts = url.split("/")
+    seo   = parts[-2]
+    parts = parts[-1].split("-")
+    tvid = parts[0]
+    link = myURL1.format(tvid, seo)
+    if len(parts) > 1:
+        part_id = parts[1]
+        link = myURL2.format(tvid, part_id, seo)
+    return link
 
 def get_m3u8_url(url):
-	link = reconstruct_link(url)
-	data = requests.get(link, headers=fake_header).text
-	sys.stderr.write(data)
-	data = json.loads(data)
-	m3u8_url = data['data']['url']
-	title    = data['data']['part_title']
-	m3u8_url = "https:" + m3u8_url
-	title = title.replace("/", "_")
-	title = title.replace(" ", "_")
-	title = title.replace("?", "_")
-	return m3u8_url, title
+    link = reconstruct_link(url)
+    data = requests.get(link, headers=fake_header).text
+    sys.stderr.write(data)
+    data = json.loads(data)
+    m3u8_url = data['data']['url']
+    title    = data['data']['part_title']
+    m3u8_url = "https:" + m3u8_url
+    title = title.replace("/", "_")
+    title = title.replace(" ", "_")
+    title = title.replace("?", "_")
+    return m3u8_url, title
 
 def kantv_download(url):
     url, name = get_m3u8_url(url)
